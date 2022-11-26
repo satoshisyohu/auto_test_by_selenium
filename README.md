6b7476006b03b82e3ec58c92e60b2c659cfb167f
satoshisyohu <satoshisyohu@gmail.com>
2022-11-26 23:23:06 +0900

    add chrome driver and logic

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # auto_test_by_selenium
 
 this is a test repository for auto tests
+
+1. csvファイル or json ファイルからjobを実行する
+2. クロームを開いてクリックを実行する
+   - htmlのinputのところを探してname="hoge"を探して
+   - find_element(By.NAME, 'hoge')のかたちで検索を実施する
+3. 実行後に終了したことを結果として受け取る
+4. Loggingでログを取得する
+5. gcsにファイルが存在していることを確認する
--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
+
+CHROME_DRIVER = "./chromedriver.exe"
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    # optionsを設定することで、selenium実行終了後にchromeが落ちないようになる。(設定しなければ落ちる)
+    browser = chrome_setting()
+    # アクセスするブラウザを設定する
+    browser.get("https://github.com/satoshisyohu/auto_test_by_selenium/tree/main")
+    elem = browser.find_element(By.NAME, 'q')
+    elem.send_keys("satoshisyohu/banking" + Keys.RETURN)
+    # browser.quit()
     return 'Hello World!'
+
+
+def chrome_setting():
+    options = Options()
+    options.add_experimental_option('detach', True)
+    return webdriver.Chrome(CHROME_DRIVER, options=options)
 
 
 if __name__ == '__main__':
